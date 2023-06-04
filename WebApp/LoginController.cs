bb9390c3f967e5d835a23598aeda0457602ea5c4
makenosense999 <71461913+makenosense999@users.noreply.github.com>
2023-06-04 22:15:58 +0300

    Add files via upload

---
+++ WebApp/LoginController.cs
@@ -0,0 +1,40 @@
+﻿using System.Security.AccessControl;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace WebApp
+{
+    [Route("api")]
+    public class LoginController : Controller
+    {
+        private readonly IAccountDatabase _db;
+
+        public LoginController(IAccountDatabase db)
+        {
+            _db = db;
+        }
+
+        [HttpPost("sign-in")]
+        public async Task<IActionResult> Login(string userName)
+        {
+            var account = await _db.FindByUserNameAsync(userName);
+            if (account != null)
+            {
+                //TODO 1: Generate auth cookie for user 'userName' with external id
+                var externalId = account.ExternalId;
+                var authCookie = GenerateAuthCookie(userName, externalId);
+                Response.Cookies.Append("AuthCookie", authCookie);
+
+                return Ok();
+            }
+            //TODO 2: return 404 if user not found
+
+            return NotFound();
+        }
+
+        private string GenerateAuthCookie(string userName, string externalId)
+        {
+            return "cookie";
+        }
+    }
+}
